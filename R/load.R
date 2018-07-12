7d4bca65a3cc5601214ae087387166a59561f24c
bbest <ben@ecoquants.com>
2018-07-12 10:51:09 -0700

    load_park_tables with machine-specific dir_csv

--- R/load.R
+++ R/load.R
@@ -9,31 +9,51 @@
 #' @export
 #'
 #' @examples
-read_park_tables <- function(nps_config, park, tbls=NULL, append_park=F){
+load_park_tables <- function(nps_config, park, tbls=NULL, append_park=F){
   #park <- "CABR"; append_park <- F
 
-  dir_csv  <- file.path(nps_config$dir_tables, park)
-  csvs     <- list.files(dir_csv, ".*\\.csv")
-  tbls_all <- path_ext_remove(csvs)
+  dir_tables <- get_dir_tables(nps_config)
+
+  dir_park        <- file.path(dir_tables, park)
+  dir_shared      <- file.path(dir_tables, "shared")
+  csvs_park       <- list.files(dir_park, ".*\\.csv")
+  csvs_shared     <- list.files(dir_shared, ".*\\.csv")
+  tbls_park_all   <- path_ext_remove(csvs_park)
+  tbls_shared_all <- path_ext_remove(csvs_shared)
+
   if (is.null(tbls))
-    tbls <- tbls_all
-  tbls_missing <- setdiff(tbls, tbls_all)
+    tbls <- tbls_park_all
+  tbls_missing <- setdiff(tbls, c(tbls_park_all, tbls_shared_all))
   if (length(tbls_missing) > 0)
-    stop(glue("Table(s) not found in {dir_csv}: {paste(tbls_missing, collapse=', ')}"))
+    stop(glue("Table(s) not found in {nps_config$dir_tables}/[{park}|shared]: {paste(tbls_missing, collapse=', ')}"))
 
   for (tbl in tbls){ # csv <- csvs[1]
-    csv <- glue("{tbl}.csv")
+
+    dir_csv <- if_else(tbl %in% tbls_park_all, dir_park, dir_shared)
+    csv <- glue("{dir_csv}/{tbl}.csv")
     if (append_park)
       tbl <- glue("{tbl}_{park}")
 
-    df  <- read_csv(file.path(dir_csv, csv))
+    df  <- read_csv(csv)
 
     assign(tbl, df, envir=.GlobalEnv)
   }
 }
 
+get_dir_tables <- function(nps_config){
+  machine <- Sys.info()[["nodename"]]
+  machine_in_config <- ifelse(machine %in% names(nps_config$dir_tables_csv), T, F)
 
-#' Get species richness plots
+  dir_tables <- case_when(
+    machine_in_config ~ nps_config$dir_tables_csv[[machine]],
+    TRUE ~ nps_config$dir_tables_csv$default)
+
+  dir_tables
+}
+
+
+
+#' Get species richness table by park and year
 #'
 #' @param park not yet implemented
 #' @param year not yet implemented
@@ -42,7 +62,7 @@ read_park_tables <- function(nps_config, park, tbls=NULL, append_park=F){
 #' @export
 #'
 #' @examples
-get_spp_richness_plots <- function(park, year){
+get_spp_richness_table <- function(park, year){
 
   read_park_tables(tbls=c("tbl_Phenology_Species", "tlu_Richness"))
 
